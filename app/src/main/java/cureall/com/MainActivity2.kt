f47745829677408eec9968b5aa303a74ff65aa19
Minoor01 <minoor01@gmail.com>
2022-12-21 23:27:40 +0600

    fixed bugs and need to add category files from database

--- app/src/main/java/cureall/com/MainActivity2.kt
+++ app/src/main/java/cureall/com/MainActivity2.kt
@@ -2,10 +2,16 @@ package cureall.com
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity2 : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val recyclerview = findViewById<RecyclerView>(R.id.recyclerView)
+         recyclerview.layoutManager = LinearLayoutManager(this,LinearLayoutManager,0,false)
+
     }
 }
